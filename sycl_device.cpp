e85dd90c977eef9509562e7520bc9db7416dc0b7
N. Pattakos <pattakosn@fastmail.com>
2023-10-13 22:49:39 +0200

    platform/device info example

--- sycl_device.cpp
+++ sycl_device.cpp
@@ -23,21 +23,41 @@ auto intel_gpu_selector2 = [](const sycl::device &dev) {
   }
   return -1;
 };
+
 int main(void)
 {
-	//TEST_CASE("intel_gpu_device_selector", "device_selectors_solution")
-
 	int a = 18, b = 24, r = 0;
 
 	try {
-		auto platforms = platform::get_platforms();
+		auto platforms = sycl::platform::get_platforms();
 
+		for( const auto& platform : platforms){
+			std::cout << "Found platform \"" << platform.get_info<sycl::info::platform::name>();
+			std::cout << "\" from vendor \"" << platform.get_info<sycl::info::platform::vendor>();
+			std::cout << "\" version \"" << platform.get_info<sycl::info::platform::version>();
+			std::cout << "\" profile \"" << platform.get_info<sycl::info::platform::profile>() << "\"" << std::endl;
+			//std::cout << " , extensions: " << platform.get_info<sycl::info::platform::extensions>() <<std::endl;
+
+			for (auto device : platform.get_devices())
+            	std::cout << "\tdevice \""<< device.get_info<sycl::info::device::name>() << "\"" << std::endl;
+		}
+
+		{
+			auto q = sycl::queue{sycl::default_selector_v};
+			std::cout << "default device on this system: \"" << q.get_device().get_info<sycl::info::device::name>() << "\"" << std::endl;
+		}
+		{
+			auto q = sycl::queue{sycl::gpu_selector_v};
+			std::cout << "default GPU device on this system: \"" << q.get_device().get_info<sycl::info::device::name>() << "\"" << std::endl;
+		}
+				{
+			auto q = sycl::queue{sycl::cpu_selector_v};
+			std::cout << "default CPU device on this system: \"" << q.get_device().get_info<sycl::info::device::name>() << "\"" << std::endl;
+		}
 		auto defaultQueue1 = sycl::queue{intel_gpu_selector1};
 		auto defaultQueue2 = sycl::queue{intel_gpu_selector2};
 
-		std::cout << "Chosen device: "
-			<< defaultQueue1.get_device().get_info<sycl::info::device::name>()
-			<< std::endl;
+		std::cout << "Chosen device: \"" << defaultQueue1.get_device().get_info<sycl::info::device::name>() << "\""	<< std::endl;
 
 		{
 			auto bufA = sycl::buffer{&a, sycl::range{1}};
