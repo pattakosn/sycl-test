58a7fc5120fe269960f08c365bf3b39f03a865d9
N. Pattakos <pattakosn@fastmail.com>
2023-10-25 17:36:41 +0200

    same example as a_plus_b only that now data initialization is done directly on device

--- a_plus_b_init_on_dev.cpp
+++ a_plus_b_init_on_dev.cpp
@@ -26,15 +26,7 @@ private:
 int main(void)
 {
 	constexpr size_t N = 1024 * 1024 * 16;
-	my_timer time_all;
-
 try{
-	for ( size_t i = 0.; i < N; ++i ) {
-		a[i] = i;
-		b[i] = 2. * i;
-	}
-	time_all.stop("Data initialization: ");
-
 	{
 		auto q = sycl::queue{sycl::default_selector_v};
 		std::cout << "default device on this system: \"" << q.get_device().get_info<sycl::info::device::name>() << "\"" << std::endl;
@@ -47,33 +39,49 @@ try{
 		auto q = sycl::queue{sycl::cpu_selector_v};
 		std::cout << "default CPU device on this system: \"" << q.get_device().get_info<sycl::info::device::name>() << "\"" << std::endl;
 	}
-
-	time_all.start();
-	auto q = sycl::queue{sycl::gpu_selector_v};
+	
+	auto q = sycl::queue{sycl::cpu_selector_v};
 	std::cout << "\nChosen device: \"" << q.get_device().get_info<sycl::info::device::name>() 
 		<< "\", max compute units: " << q.get_device().get_info<sycl::info::device::max_compute_units>()
 		<< std::endl;
+	
+	my_timer time_all;
+	sycl::buffer<int> bufA{sycl::range{N}};
+	sycl::buffer<int> bufB{sycl::range{N}};
+	sycl::buffer<int> bufR{sycl::range{N}};
 
-	auto bufA = sycl::buffer{a.data(), sycl::range{a.size()}};
-	auto bufB = sycl::buffer{b.data(), sycl::range{b.size()}};
-	auto bufR = sycl::buffer{r.data(), sycl::range{r.size()}};
+	time_all.start();
+	q.submit([&](sycl::handler &cgh) {
+		auto aA = sycl::accessor{bufA, cgh, sycl::write_only, sycl::no_init};
+		auto aB = sycl::accessor{bufB, cgh, sycl::write_only, sycl::no_init};
+		//auto aR = sycl::accessor{bufR, cgh, sycl::write_only, sycl::no_init};
+		cgh.parallel_for(N, [=](sycl::id<1> id) {
+			aA[id] = id;
+			aB[id] = 2*id;
+		});
+	}).wait_and_throw();
+	time_all.stop("Data initialization: ");
 //for (int i =0; i < 1024;i++)
 	q.submit([&](sycl::handler &cgh) {
-		auto accA = sycl::accessor{bufA, cgh, sycl::read_only};
-		auto accB = sycl::accessor{bufB, cgh, sycl::read_only};
-		auto accR = sycl::accessor{bufR, cgh, sycl::write_only};
-
-		cgh.parallel_for<vector_add>(sycl::range(N), [=](sycl::id<1> id) { accR[id] = accA[id] + accB[id]; });
-	});
+		auto aA = sycl::accessor{bufA, cgh, sycl::read_only};
+		auto aB = sycl::accessor{bufB, cgh, sycl::read_only};
+		auto aR = sycl::accessor{bufR, cgh, sycl::read_write};
+		cgh.parallel_for(N, [=](sycl::id<1> id) {
+			aR[id] = aA[id] + aB[id];
+		});
+	}).wait_and_throw();
+	time_all.stop("After crunching: ");
 	
-	q.wait_and_throw();
-	time_all.stop("Data crunching: ");
+	sycl::host_accessor end_A{bufA, sycl::read_only};
+	sycl::host_accessor end_B{bufB, sycl::read_only};
+	sycl::host_accessor end_R{bufR, sycl::read_only};
+	
+	for( int i = 0; i < 8*16; i+=16 )//N; i+= (1024*1024))
+		std::cout << " a[" << i << "]= " << end_A[i] << "\t+ b[" << i << "]= " << end_B[i] << "\t= c["<< i << "]= " << end_R[i] << std::endl;
 } catch(const sycl::exception &e) {
  	std::cout << "SyCL exception caught: " << e.what() << std::endl;
 } catch(const std::exception& e) {
 	std::cout << "STD exception caught: " << e.what() << std::endl;
 }
-	for( int i = 0; i < 8*16; i+=16 )//N; i+= (1024*1024))
-		std::cout << " a[" << i << "]= " << a[i] << "\t+ b[" << i << "]= " << b[i] << "\t= c["<< i << "]= " << r[i] << std::endl;
 	return EXIT_SUCCESS;
 }
